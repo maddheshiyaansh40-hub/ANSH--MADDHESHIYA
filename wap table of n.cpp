823bd698732cfe4ac2ac28685f521723c4777319
maddheshiyaansh40-hub <maddheshiyaansh40@gmail.com>
2025-11-01 16:12:42 +0530

    msg

---
+++ wap table of n.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+int i=1,n;
+while(i<=10)
+{
+     cin>>n;
+    cout<<"n";
+   cout<<n<<"n"<<i<<"="<<(n*i)<<std::endl;
+   i++;
+}
+return 0;
+}
