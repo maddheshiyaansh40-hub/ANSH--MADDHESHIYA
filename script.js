62a0508e0f3967daf4881f927f6100f16bab31a9
maddheshiyaansh40-hub <maddheshiyaansh40@gmail.com>
2026-02-24 12:14:59 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log(" god understand my feeling");
+var varriable = "hello world";
+var age = 20;
+var isStudent = true;
+var hobbies = ["reading", "coding", "gaming"];  
+var person = {
+    name: "John",
+    age: 30,
+    isStudent: false
+};  
+function greet(name) {
+    return "Hello, " + name + "!";
+}   
+//data types in js ;
+var stringVar = "This is a string";
+var numberVar = 42;
+var booleanVar = true;  
+var arrayVar = [1, 2, 3, 4, 5]; 
+var objectVar = {
+    name: "Alice",
+    age: 25,
+    isStudent: true
+};  
+var nullVar = null; 
+var undefinedVar;
+console.log(objectVar.name,age);
